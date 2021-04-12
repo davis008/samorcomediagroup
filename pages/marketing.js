6bdd015de8b6a3d5ac45747d129eee0a8de909c4
Davis <davisomondi008@gmail.com>
2021-04-12 13:30:40 +0300

    Create marketing.js

---
+++ pages/marketing.js
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout"
+const Marketing=()=>(
+    
+    <Layout>
+        <br/>
+      <img src="./bannner.jpg" class="img-fluid" alt="Responsive image"></img>
+    </Layout>
+);
+export default Marketing;
