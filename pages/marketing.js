896efc0397d79cd713b8e33547aaa5ad7d64791c
Davis <davisomondi008@gmail.com>
2021-05-28 11:48:03 +0300

    Update marketing.js

--- pages/marketing.js
+++ pages/marketing.js
@@ -8,8 +8,9 @@ const Marketing=()=>(
         <br/>
     <Container>
   <Row>
-    <Col><img src="./barner1.png" class="img-fluid"  width="800" height="400" alt="Responsive image"></img></Col>
-    <Col><img src="./bannner.jpg" class="img-fluid"   width="800" height="400" alt="Responsive image"></img></Col>
+    <Col><img src="./barner1.png" class="img-fluid"  alt="Responsive image"></img></Col>
+    <Col><img src="./1.png" class="img-fluid"    alt="Responsive image"></img></Col>
+    <Col><img src="./2.png" class="img-fluid"   alt="Responsive image"></img></Col>
   </Row>
 </Container>
    
