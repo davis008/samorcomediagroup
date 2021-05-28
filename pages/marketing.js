696c16d86154f006f7c9d3aaa336104bb63e05ee
Davis <davisomondi008@gmail.com>
2021-05-28 10:08:46 +0300

    Update marketing.js

--- pages/marketing.js
+++ pages/marketing.js
@@ -1,10 +1,19 @@
 import Layout from "../components/Layout"
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
 const Marketing=()=>(
     
     <Layout>
         <br/>
-    <p>a</p>
-      <img src="./bannner.jpg" class="img-fluid" alt="Responsive image"></img>
+    <Container>
+  <Row>
+    <Col><img src="./barner1.jpg" class="img-fluid" alt="Responsive image"></img></Col>
+    <Col><img src="./bannner.jpg" class="img-fluid" alt="Responsive image"></img></Col>
+  </Row>
+</Container>
+   
+      
     </Layout>
 );
 export default Marketing;
