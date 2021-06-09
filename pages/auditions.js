af1d48da946965485b951dc6159818a2c5144ae2
Davis <davisomondi008@gmail.com>
2021-06-09 18:51:36 +0300

    Create auditions.js

---
+++ pages/auditions.js
@@ -0,0 +1,51 @@
+import Layout from "../components/Layout"
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import InputGroup from 'react-bootstrap/InputGroup'
+import Button from 'react-bootstrap/Button'
+
+const Auditions=()=>(
+    
+    <Layout>
+        <br/>
+  <Form>
+  
+   
+  <InputGroup className="mb-3">
+    <InputGroup.Prepend>
+      <InputGroup.Text id="basic-addon1">FullName</InputGroup.Text>
+    </InputGroup.Prepend>
+    <FormControl
+      placeholder="Fullname"
+      aria-label="  Fullname"
+      aria-describedby="basic-addon1"
+    />
+  </InputGroup>
+  
+  <Form.Group controlId="formBasicPhone">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="tel" placeholder="Enter Phone" />
+  </Form.Group>
+ 
+  <Form.Group controlId="formBasicLocation">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="text" placeholder="Enter eLocation" />
+  </Form.Group>
+ 
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+ 
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+   
+      
+    </Layout>
+);
+export default Auditions;
