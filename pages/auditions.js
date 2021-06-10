c7fd04bac11469ba9351b055b67fb1c472232e3d
Davis <davisomondi008@gmail.com>
2021-06-10 18:58:44 +0300

    Update auditions.js

--- pages/auditions.js
+++ pages/auditions.js
@@ -3,57 +3,43 @@ import Form from 'react-bootstrap/Form'
 import FormControl from 'react-bootstrap/FormControl'
 import InputGroup from 'react-bootstrap/InputGroup'
 import Button from 'react-bootstrap/Button'
-import { useForm} from '@formspree/react';
+import { useForm, ValidationError } from '@formspree/react';
 
 
 function Auditions(){
-    const [state, handleSubmit] = useForm("xpzkrwbz");
+    
+  const [state, handleSubmit] = useForm("xpzkrwbz");
   if (state.succeeded) {
-      return <p>Thanks for sending details!</p>;
+      return <p>Thanks for joining!</p>;
   }
-    return (
-    <Layout>
-        <br/>
-  <Form onSubmit={handleSubmit}>
-  
-   
-  <InputGroup className="mb-3">
-    <InputGroup.Prepend>
-      <InputGroup.Text id="basic-addon1">FullName</InputGroup.Text>
-    </InputGroup.Prepend>
-    <FormControl
-      name="name"
-      placeholder="Fullname"
-      aria-label="  Fullname"
-      aria-describedby="basic-addon1"
-    />
-  </InputGroup>
-  
-  <Form.Group controlId="formBasicPhone">
-    <Form.Label>Phone</Form.Label>
-    <Form.Control type="tel" name="tel" placeholder="Enter Phone" />
-  </Form.Group>
- 
-  <Form.Group controlId="formBasicLocation">
-    <Form.Label>Residence</Form.Label>
-    <Form.Control type="text" name="location" placeholder="Enter Location" />
-  </Form.Group>
- 
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" name="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
- 
-  <Button variant="primary" type="submit" disabled={state.submitting}>
-    Submit
-  </Button>
-</Form>
-   
-      
-    </Layout>
+  return (
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Email Address
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      <textarea
+        id="message"
+        name="message"
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+    </form>
 );
 }
 export default Auditions;
